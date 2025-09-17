cb274326870334948527c0098a1697ee9ad8cfc7
GhOsT8990asd <parcoursik4@gmail.com>
2025-09-17 16:23:34 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -13,12 +13,12 @@ def run():
     password = "5002207f2329a5481a580f69a11a0f54b4b83875"
     auth_url = f"https://alimer-comert-co.syrve.online/resto/api/auth?login={login}&pass={password}"
     token = requests.get(auth_url).text.strip()
-
+    
     # 📊 Получение OLAP-отчёта
     olap_url = (
         "https://alimer-comert-co.syrve.online/resto/api/v2/reports/olap/byPresetId/"
         "0f032c52-7afb-4b48-87ae-79c5f3ebdfa4"
-        f"?key={token}&dateFrom=2025-04-01&dateTo=2025-10-01"
+        f"?key={token}&dateFrom=2025-04-01&dateTo=datetime.today().strftime("%Y-%m-%d")"
     )
     data = requests.get(olap_url).json()
 
